b1036e5d90e04ab438db693f8ae564258d4b6e9f
Patrisson <aman2833617@gmail.com>
2024-07-04 19:41:35 +0530

    Project structure and interface creation done

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button} from '@mui/material';
+
+
+const App = () => {
+  return (
+
+    <div>
+      <h1>Hello from react app</h1>
+      <Button variant="contained" color="primary">Mui button</Button>
+    </div>
+
+  )
+}
+
+export default App
